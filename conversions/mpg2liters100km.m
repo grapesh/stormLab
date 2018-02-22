dce1a51f7ce9969d1bbed9b6f594c5c1752c9b0c
Sergey Vinogradov <grapesh@gmail.com>
2018-02-22 16:21:39 -0500

    avoid division by 0

--- conversions/mpg2liters100km.m
+++ conversions/mpg2liters100km.m
@@ -1,11 +1,14 @@
 % 20160804 :: grapesh@gmail.com
-% Notorious formula for Easter Europeans
+% Notorious formula for Eastern Europeans
 % curious about fuel consumption.
 % mpg --> liters/100km
 % 
 function out = mpg2liters100km (in)
 
-out = sm2km(in)/gal2liters(1); % km/liter
-out = 100/out;  % liters / 100 km
+out = 0;
+if in ~= 0   
+  out = sm2km(in)/gal2liters(1); % km/liter
+  out = 100/out;  % liters / 100 km
+end
 
 end
